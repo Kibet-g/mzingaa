a9123f78e2ab28f4c2081adc527eb4b4e0a49afc
KIBET KORIR <93614316+Kibet-g@users.noreply.github.com>
2023-06-05 15:23:58 +0300

    Started My Dart Project for an ecommerce liqour application

--- lib/home/liqour_page_body.dart
+++ lib/home/liqour_page_body.dart
@@ -12,101 +12,136 @@ class LiqourPageBody extends StatefulWidget {
 }
 
 class _LiqourPageBodyState extends State<LiqourPageBody> {
-  PageController pageController=PageController(viewportFraction: 0.85 );
+  PageController pageController = PageController(viewportFraction: 0.85);
+  var currPageValue = 0.0;
+  double scaleFactor = 0.0;
+  double _height = 220;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        currPageValue = pageController.page!;
+        print("Current value is " + currPageValue.toString());
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.redAccent,
-      height: 320, // Set a specific height for the PageView
+      height: 320,
       child: PageView.builder(
         controller: pageController,
         itemCount: 5,
         itemBuilder: (context, position) {
-          return _buildPageItem(position);//to return the scrolling effecr
+          return _buildPageItem(position);
         },
       ),
     );
-
   }
 
   Widget _buildPageItem(int index) {
-    //we build the container first with height margin and padding
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: const EdgeInsets.only(left: 10, right: 10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: index.isEven ? const Color(0xFF800080) : const Color(0xFF4682B4),//decision maker to use a specific color
-            image: const DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage("assets/Image/alcohol.jpg"),
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 120,
-            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+    Matrix4 matrix = Matrix4.identity();
+    if (index == currPageValue.floor()) {
+      var currScale = 1 - (currPageValue - index) * (1 - scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == currPageValue.floor() + 1) {
+      var currScale = scaleFactor + (currPageValue - index + 1) * (1 - scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    } else if (index == currPageValue.floor() - 1) {
+      var currScale = 1 - (currPageValue - index) * (1 - scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currScale = 0.0;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height * (1 - scaleFactor) / 2, 1);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: const EdgeInsets.only(left: 10, right: 10),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
-              color: Colors.white,
-
-
-            ),
-            child: Container(
-              padding: EdgeInsets.only(top: 10, left: 15, right: 15),
-              child: Column (
-                //catering for all things to be centered
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BigText(text: "Gilbeys", color: Colors.black,),
-                  SizedBox(height: 10,),
-                  Row(
-                    children: [
-                      Wrap(
-                        children: List.generate(5, (index) => Icon(Icons.star, color: AppColors.turquoiseColor, size: 15,)),
-                      ),
-                      SizedBox(width: 10,),
-                      SmallText(text: "4.5"),
-                      SizedBox(width: 10,),
-                      SmallText(text: "1287"),
-                      SizedBox(width: 10,),
-                      SmallText(text: "comments"),
-
-                    ],
-                  ),
-                  SizedBox(height: 20,),
-                  Row(
-                    children: [
-                      IconAndTextWidget(icon: Icons.circle_sharp,
-                          text: "Normal",
-
-                          iconcolor: AppColors.orangeColor,
-                      ),
-
-                      IconAndTextWidget(icon: Icons.location_on,
-                        text: "1.7KM",
-
-                        iconcolor: AppColors.turquoiseColor,
-                      ),
-                      IconAndTextWidget(icon: Icons.access_time_rounded,
-                        text: "32min",
-
-                        iconcolor: AppColors.redColor,
-                      ),
-
-                    ],
-                  )
-
-                ],
+              color: index.isEven ? const Color(0xFF800080) : const Color(0xFF4682B4),
+              image: const DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/Image/alcohol.jpg"),
               ),
             ),
           ),
-        )
-      ],
-    ) ;
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Gilbeys", color: Colors.black,),
+                    SizedBox(height: 10,),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) => Icon(Icons.star, color: AppColors.turquoiseColor, size: 15,)),
+                        ),
+                        SizedBox(width: 10,),
+                        SmallText(text: "4.5"),
+                        SizedBox(width: 10,),
+                        SmallText(text: "1287"),
+                        SizedBox(width: 10,),
+                        SmallText(text: "comments"),
+                      ],
+                    ),
+                    SizedBox(height: 20,),
+                    Row(
+                      children: [
+                        IconAndTextWidget(
+                          icon: Icons.circle_sharp,
+                          text: "Normal",
+                          iconcolor: AppColors.orangeColor,
+                        ),
+                        IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: "1.7KM",
+                          iconcolor: AppColors.turquoiseColor,
+                        ),
+                        IconAndTextWidget(
+                          icon: Icons.access_time_rounded,
+                          text: "32min",
+                          iconcolor: AppColors.redColor,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
